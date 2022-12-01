f44fd57810077d6910305d697287a7a44794e5bc
Marcus Dahlstein <marcus.dahlstein@leovegas.com>
2022-12-01 09:44:14 +0100

    Day 1

---
+++ day_1/main.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+all = []
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+    a = []
+    for l in lines:
+        if l == '':
+            all.append(a)
+            a = []
+        else:
+            a.append(int(l))
+    all.append(a)
+
+def part_1():
+    print(max(list(map(lambda x: sum(x), all))))
+
+def part_2():
+    q=list(map(lambda x: sum(x), all))
+    q.sort(reverse=True)
+    print(sum(q[0:3]))
+
+     
+
+part_2()    
+
+    
