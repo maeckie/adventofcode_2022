1029eda1beb560bbd12d7cbdbf2ec1a809cdcdb0
Marcus Dahlstein <marcus.dahlstein@leovegas.com>
2022-12-04 09:56:48 +0100

    No more hellish ifs...

--- day_4/main.py
+++ day_4/main.py
@@ -3,15 +3,13 @@ p1_overlaps = 0
 p2_overlaps = 0
 for areas in lines:
     elv_areas = list(map(lambda x: x.split('-'), areas.split(',')))
-    if  (int(elv_areas[0][0]) >= int(elv_areas[1][0]) and int(elv_areas[0][1]) <= int(elv_areas[1][1]) or
-        int(elv_areas[1][0]) >= int(elv_areas[0][0]) and int(elv_areas[1][1]) <= int(elv_areas[0][1]) 
-        ):
+    left = range(int(elv_areas[0][0]), int(elv_areas[0][1])+1)
+    right = range(int(elv_areas[1][0]), int(elv_areas[1][1])+1)
+    if all(elem in left  for elem in right) or all(elem in right  for elem in left):
         p1_overlaps += 1
-    if (int(elv_areas[0][0]) <= int(elv_areas[1][1]) and int(elv_areas[0][1]) >= int(elv_areas[1][0]) or
-        int(elv_areas[1][0]) <= int(elv_areas[0][1]) and int(elv_areas[1][1]) >= int(elv_areas[0][0])
-        ):
-        p2_overlaps += 1
 
+    if any(elem in left  for elem in right):
+        p2_overlaps += 1        
 
 print(f"Part1: {p1_overlaps}")
 print(f"Part2: {p2_overlaps}")
